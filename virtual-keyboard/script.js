698198ad77b47d84fbbd5569752c4da2c0696ad0
Ivan Kasenka <kasenka.ivan@gmail.com>
2023-05-01 00:06:24 +0300

    feat: ended main part css styles

--- virtual-keyboard/script.js
+++ virtual-keyboard/script.js
@@ -1,7 +1,38 @@
-import {createEmptyKeyboard} from './startKeyboard.js';
+import { createlayoutKeyboard } from './startKeyboard.js';
+
+
+const functionalKeys = [16];
+
+
 
 initialize();
-
 function initialize() {
-  createEmptyKeyboard();
+  createlayoutKeyboard();
+
 }
+
+
+addEventListener("keydown", (event) => {
+  document.getElementById(event.keyCode).parentNode.classList.add("click");
+  if(document.activeElement.classList[0] !==  "textAreaField" && !functionalKeys.includes(event.keyCode)) {
+    const textArea = document.getElementsByClassName("textAreaField")[0];
+    textArea.value = textArea.value + event.key;
+  }
+  if(event.keyCode === 16) {
+    document.getElementById("49").innerHTML = "!";
+  }
+
+});
+
+addEventListener("keyup", (event) => {
+  setTimeout(() => 
+  {
+    document.getElementById(event.keyCode).parentNode.classList.remove("click");
+    if(event.keyCode === 16) {
+      document.getElementById("49").innerHTML = "1";
+    }
+  }
+  , 300)
+  
+});
+
